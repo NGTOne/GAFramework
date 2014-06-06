0b4606b4f73680f138ba0c310357d13aee2720aa
Sebastian Lenartowicz <slenarto@uoguelph.ca>
2014-06-05 21:18:04 -0400

    [Property]: Added <int> cases to Property
    Gave Property the tools to deal with ints. Will add other types later.
    
    Signed-off-by: Sebastian Lenartowicz <slenarto@uoguelph.ca>

--- src/core/Property.cpp
+++ src/core/Property.cpp
@@ -11,15 +11,33 @@ template <class T> Property<T>::Property(T * newProperty) {
 	myProperty = newProperty;
 }
 
+template <> Property<int>::Property(int * newProperty) {
+	myProperty = newProperty;
+}
+
 template <class T> Property<T>::~Property() {
 	if (myProperty != NULL) {
 		delete(myProperty);
 	}
 }
 
+template <> Property<int>::~Property() {
+	if (myProperty != NULL) {
+		free(myProperty);
+	}
+}
+
 template <class T> void Property<T>::setProperty(T * newProperty) {
 	if (myProperty != NULL) {
 		delete(myProperty);
+	}
+
+	myProperty = newProperty;
+}
+
+template <> void Property<int>::setProperty(int * newProperty) {
+	if (myProperty != NULL) {
+		free(myProperty);
 	}
 
 	myProperty = newProperty;
