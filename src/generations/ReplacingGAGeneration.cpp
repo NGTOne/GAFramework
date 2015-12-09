9537db32715fb5882474196b81e988e02c09277d
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2015-12-09 16:00:20 -0500

    [ReplacingGAGeneration]: Use local elitism

--- src/generations/ReplacingGAGeneration.cpp
+++ src/generations/ReplacingGAGeneration.cpp
@@ -44,63 +44,25 @@ Individual ** ReplacingGAGeneration::breedMutateSelect(Individual ** initialPopu
 
 	for (int i = 0; i < populationSize; i++) newPopulation[i] = initialPopulation[i];
 
-	uniform_real_distribution<double> replacementDistribution(0, 1);
-
 	for (int i = 0; i < populationSize/2; i++) {
 		for (int k = 0; k < 2; k++) {
 			indexes[k] = getParent(populationFitnesses, populationSize);
 		}
-		firstParent = newPopulation[indexes[0]];
-		secondParent = newPopulation[indexes[1]];
+		firstParent = initialPopulation[indexes[0]];
+		secondParent = initialPopulation[indexes[1]];
 
 		children = firstParent->crossoverOperation(secondParent);
 
 		for (int k = 0; k < 2; k++) {
-			if (replacementDistribution(generator) < replacementRate) {
-				newPopulation[indexes[k]] = children[k]->mutationOperation();
-			} else {
-				delete(children[k]);
+			children[k] = children[k]->mutationOperation();
+
+			if (children[k]->checkFitness() > newPopulation[indexes[k]]->getFitness()) {
+				newPopulation[indexes[k]] = children[k];
 			}
-			newPopulation[indexes[k]]->getFitness();
 		}
 
 		free(children);
 	}
 
-	//free(initialPopulation);
 	return(newPopulation);
-
-	/*for(;numOffspring < populationSize;) {
-		firstIndex = getParent(populationFitnesses, populationSize);
-		secondIndex = getParent(populationFitnesses, populationSize);
-
-		firstParent = initialPopulation[firstIndex];
-		secondParent = initialPopulation[secondIndex];
-
-		children = firstParent->crossoverOperation(secondParent);
-
-		firstParent = children[0]->mutationOperation();
-		secondParent = children[1]->mutationOperation();
-
-		delete(children[0]);
-		delete(children[1]);
-		free(children);
-
-		newPopulation[numOffspring] = firstParent;
-		newFitnesses[numOffspring++] = firstParent->getFitness();
-
-		if (numOffspring < populationSize) {
-			newPopulation[numOffspring] = secondParent;
-			newFitnesses[numOffspring++] = secondParent->getFitness();
-		}
-	}*/
-
-	/*for (int i = 0; i < populationSize; i++) {
-		populationFitnesses[i] = newFitnesses[i];
-		delete(initialPopulation[i]);
-	}
-
-	free(newFitnesses);
-
-	return newPopulation;*/
 }
