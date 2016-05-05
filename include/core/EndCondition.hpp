bd2ecae53477d1729dcd437120bc57bc93004ff1
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-05-05 15:27:52 -0400

    [EndCondition]: Tweaked inputs to EndCondition

--- include/core/EndCondition.hpp
+++ include/core/EndCondition.hpp
@@ -1,26 +1,17 @@
-#include "Individual.hpp"
+#include "Solution.hpp"
+#include "PopulationNode.hpp"
+#include <vector>
 #pragma once
-
-/*
-* This abstract class allows us to specify an ending condition for the
-* genetic algorithm other than having a number of generations go by.
-* This can be something like having an individual in the population reach
-* a given fitness, or the population as a whole reaching some milestone.
-*/
 
 class EndCondition {
 	private:
 
 	protected:
-	virtual bool checkIndividual(Individual * target)=0;
+	virtual bool checkSolution(Solution * target)=0;
 	
 	public:
 	EndCondition();
-
 	~EndCondition();
 
-	virtual bool checkCondition(
-		Individual ** population,
-		int populationSize
-	)=0;
+	virtual bool checkCondition(PopulationNode * node)=0;
 };
