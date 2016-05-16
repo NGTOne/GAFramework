417a768ffa5f83235f23c98e807893cd1645c823
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-05-15 20:15:37 -0400

    [UniformCrossover]: Refactored UniformCrossover

--- include/crossovers/UniformCrossover.hpp
+++ include/crossovers/UniformCrossover.hpp
@@ -1,31 +1,15 @@
-#include "../core/CrossoverOperation.hpp"
+#include "../nodes/EANode/CrossoverOperation.hpp"
 #pragma once
-
-/*
-* The uniform crossover operator randomly generates a bitwise mask, and then
-* crosses over only the genes that are masked.
-*
-* EXAMPLE:
-* Mask:        01110101
-* Parent 1:    ABBBDCCB
-* Parent 2:    BBCDABDA
-* Offspring 1: ABCDDBCA
-* Offspring 2: BBBBACDB
-*/
 
 class UniformCrossover : public CrossoverOperation {
 	private:
 
 	protected:
-
-	double crossoverRate;
 	
 	public:
-	
-	UniformCrossover(double crossoverChance);
-	UniformCrossover(double crossoverChance, unsigned newSeed);
+	UniformCrossover();
+	UniformCrossover(unsigned int numOffspring);
+	UniformCrossover(unsigned int numOffspring, unsigned seed);
 
-	Genome ** crossOver(Genome * firstGenome, Genome * secondGenome);
-
-	std::string toString();
+	std::vector<Genome*> crossOver(std::vector<Genome*> genomes)=0;
 };
--- src/crossovers/UniformCrossover.cpp
+++ src/crossovers/UniformCrossover.cpp
@@ -5,67 +5,35 @@
 
 using namespace std;
 
-UniformCrossover::UniformCrossover(double crossoverChance) : CrossoverOperation() {
-	crossoverRate = crossoverChance;
-}
+UniformCrossover::UniformCrossover() : CrossoverOperation() {}
 
-UniformCrossover::UniformCrossover(double crossoverChance, unsigned newSeed) : CrossoverOperation(newSeed) {
-	crossoverRate = crossoverChance;
-}
+UniformCrossover::UniformCrossover(
+	unsigned int numOffspring
+) : CrossoverOperation(numOffspring) {}
 
-Genome ** UniformCrossover::crossOver(Genome * firstGenome, Genome * secondGenome) {
-	int shortestGenome;
+UniformCrossover::UniformCrossover(
+	unsigned int numOffspring,
+	unsigned seed
+) : CrossoverOperation(numOffspring, seed) {}
 
-	int firstGenomeLength = firstGenome->getGenomeLength();
-	int secondGenomeLength = secondGenome->getGenomeLength();
+vector<Genome*> UniformCrossover::crossOver(vector<Genome*> genomes) {
+	int shortestGenomeLength = this->shortestGenome(genomes);
+	vector<Genome*> children;
+	vector<vector<int>> parentGenomes = this->getGenomes(genomes);
+	vector<vector<Locus*>> parentLoci = this->getLoci(genomes);
 
-	int * genomeOne = firstGenome->getGenome();
-	int * genomeTwo = secondGenome->getGenome();
+	uniform_int_distribution<int> parentDist(0, genomes.size() - 1);
 
-	if (firstGenomeLength > secondGenomeLength) {
-		shortestGenome = secondGenomeLength;
-	} else {
-		shortestGenome = firstGenomeLength;
-	}
-
-	//We're generating two children - each one uses the same crossover
-	//points, but draws from the parents in a different order
-	int ** children = (int**)malloc(sizeof(int*)*2);
-
-	children[0] = (int*)malloc(sizeof(int)*shortestGenome);
-	children[1] = (int*)malloc(sizeof(int)*shortestGenome);
-
-	uniform_real_distribution<double> distribution(0, 1);
-
-	for (int i = 0; i < shortestGenome; i++) {
-		if (distribution(generator) <= crossoverRate) {
-			children[0][i] = genomeTwo[i];
-			children[1][i] = genomeOne[i];
-		} else {
-			children[0][i] = genomeTwo[i];
-			children[1][i] = genomeOne[i];
+	for (int i = 0; i < this->numOffspring; i++) {
+		vector<int> childGenome;
+		vector<Locus*> childLoci;
+		for (int k = 0; k < shortestGenomeLength; k++) {
+			int parent = parentDist(this->generator);
+			childGenome.push_back(parentGenomes[parent][k]);
+			childLoci.push_back(parentLoci[parent][k]);
 		}
+		children.push_back(new Genome(childGenome, childLoci));
 	}
 
-	Genome ** returnChildren = (Genome**)malloc(sizeof(Genome*)*2);
-
-	returnChildren[0] = new Genome(children[0], shortestGenome, firstGenome->getGeneNodes());
-	returnChildren[1] = new Genome(children[1], shortestGenome, secondGenome->getGeneNodes());
-
-	free(children[0]);
-	free(children[1]);
-	free(children);
-
-	return returnChildren;
-}
-
-string UniformCrossover::toString() {
-	string returnString = "";
-	stringstream ss;
-
-	ss << "Type: Uniform Crossover\nRandom Seed: " << seed << "\nCrossover Rate: " << crossoverRate << "\n";
-
-	returnString = ss.str();
-
-	return returnString;
+	return children;
 }
