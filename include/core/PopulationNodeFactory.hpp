32ed0531c8f7489591e9bffe234d9ed0ffa55e03
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-07-06 12:54:09 -0400

    [PopulationNodeFactory]: Added base factory

---
+++ include/core/PopulationNodeFactory.hpp
@@ -0,0 +1,45 @@
+#include "PopulationNode.hpp"
+#include <cstdarg>
+#pragma once
+
+class PopulationNodeFactory {
+	private:
+
+	protected:
+
+	public:
+	template <PopulationNode NodeType, typename... params>
+	static PopulationNode * createNode(
+		unsigned int populationSize,
+		std::vector<Locus*> loci,
+		std::vector<ObjectiveFunction*> objectives,
+		ToStringFunction * populationToString,
+		std::vector<EndCondition*> conditions,
+		std::string nodeName,
+		unsigned int accelerationFactor,
+		params... as
+	);
+};
+
+template<PopulationNode NodeType, typename... params>
+static PopulationNode * createNode(
+	unsigned int populationSize,
+	std::vector<Locus*> loci,
+	std::vector<ObjectiveFunction*> objectives,
+	ToStringFunction * populationToString,
+	std::vector<EndCondition*> conditions,
+	std::string nodeName,
+	unsigned int accelerationFactor,
+	params... as
+) {
+	return new NodeType(
+		populationSize,
+		loci,
+		objectives,
+		populationToString,
+		conditions,
+		nodeName,
+		accelerationFactor,
+		as...
+	);
+}
