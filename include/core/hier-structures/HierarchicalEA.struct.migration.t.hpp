3d56a422e1412d580e3818125565309a6850590c
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-07-07 01:58:46 -0400

    [HierarchicalEA]: Broke up headers further

--- include/core/hier-structures/HierarchicalEA.struct.migration.t.hpp
+++ include/core/hier-structures/HierarchicalEA.struct.migration.t.hpp
@@ -1,6 +1,6 @@
-#include "HierarchicalEA.hpp"
-#include "PopulationNodeFactory.hpp"
-#include "../exception/MismatchedCountsException.hpp"
+#include "../HierarchicalEA.hpp"
+#include "../PopulationNodeFactory.hpp"
+#pragma once
 
 template<typename NodeType, typename... params>
 void HierarchicalEA::addMigratoryRing(
@@ -96,59 +96,4 @@ void HierarchicalEA::addMigratoryTorus(
 			bidirectionalVertical,
 			n
 		);
-}
-
-template<typename NodeType, typename... params>
-void HierarchicalEA::addConstructiveLattice(
-	PopulationFormula * formula,
-	std::vector<std::vector<Locus*>> contextLoci,
-	std::vector<std::vector<ObjectiveFunction*>> objectives,
-	std::vector<ToStringFunction*> toStrings,
-	std::vector<std::vector<EndCondition*>> conditions,
-	std::vector<std::vector<std::string>> names,
-	std::vector<bool> print,
-	std::vector<bool> end,
-	params... as
-) {
-	if (
-		names.size() != contextLoci.size()
-		|| (!objectives.empty() && names.size() != objectives.size())
-		|| names.size() != toStrings.size()
-		|| names.size() != conditions.size()
-		|| names.size() != print.size()
-		|| names.size() != end.size()
-	) throw MismatchedCountsException(); // TODO: Refactor this
-
-	std::vector<Locus*> levelLoci;
-	std::vector<PopulationNode*> levelNodes;
-	for (unsigned int i = names.size() - 1; i >= 0; i--) {
-		levelLoci.insert(
-			levelLoci.end(),
-			contextLoci[i].begin(),
-			contextLoci[i].end()
-		);
-
-		levelNodes = PopulationNodeFactory::createNodes<NodeType>(
-			formula->getPopulationSize(levelLoci.size()),
-			levelLoci,
-			(!objectives.empty() ? objectives[i]
-				: std::vector<ObjectiveFunction*>({})),
-			toStrings[i],
-			conditions[i],
-			names[i],
-			as...
-		);
-
-		this->addNodes(levelNodes, print[i], end[i]);
-		levelLoci.clear();
-		for (unsigned int k = 0; k < levelNodes.size(); k++)
-			levelLoci.push_back(
-				new PopulationLocus(levelNodes[k])
-			);
-	}
-
-	// Clean up - otherwise the "head" nodes will have unused loci
-	// pointing to them
-	for (unsigned int i = 0; i < levelLoci.size(); i++)
-		delete(levelLoci[i]);
 }
