b6a94014e57a9d6fbd9037c541d4b2d4d86b795a
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-07-06 21:22:59 -0400

    [HierarchicalEA]: Started breaking apart header

---
+++ include/core/HierarchicalEA.meta.t.hpp
@@ -0,0 +1,81 @@
+#include "HierarchicalEA.hpp"
+#include "meta/MetaPopulationFactory.hpp"
+
+template<typename MetaNodeType, typename... params>
+void HierarchicalEA::addMetaPopulation(
+	unsigned int populationSize,
+	std::tuple<
+		std::vector<std::string>,
+		ApportionmentFunction *,
+		AggregationFunction *
+	> nodes,
+	std::vector<ObjectiveFunction*> flattenedObjectives,
+	ToStringFunction * flattenedToString,
+	std::vector<EndCondition*> conditions,
+	std::string metaNodeName,
+	params... as
+) {
+	std::vector<std::tuple<
+		std::string,
+		ApportionmentFunction*,
+		AggregationFunction*
+	>> fullNodes;
+	for (unsigned int i = 0; i < std::get<0>(nodes).size(); i++)
+		fullNodes.push_back(std::make_tuple(
+			std::get<0>(nodes)[i],
+			std::get<1>(nodes),
+			std::get<2>(nodes)
+		));
+
+	this->addMetaPopulation<MetaNodeType>(
+		populationSize,
+		fullNodes,
+		flattenedObjectives,
+		flattenedToString,
+		conditions,
+		metaNodeName,
+		as...
+	);
+}
+
+template<typename MetaNodeType, typename... params>
+void HierarchicalEA::addMetaPopulation(
+	unsigned int populationSize,
+	std::vector<std::tuple<
+		std::string,
+		ApportionmentFunction *,
+		AggregationFunction *
+	>> nodes,
+	std::vector<ObjectiveFunction*> flattenedObjectives,
+	ToStringFunction * flattenedToString,
+	std::vector<EndCondition*> conditions,
+	std::string metaNodeName,
+	params... as
+) {
+	std::vector<std::tuple<
+		PopulationNode*,
+		ApportionmentFunction*,
+		AggregationFunction *
+	>> trueNodes;
+	for (unsigned int i = 0; i < nodes.size(); i++)
+		trueNodes.push_back(std::make_tuple(
+			this->getNodeByName(std::get<0>(nodes[i])),
+			std::get<1>(nodes[i]),
+			std::get<2>(nodes[i])
+		));
+
+	this->addNode(
+		MetaPopulationFactory::createMeta<MetaNodeType>(
+			populationSize,
+			trueNodes,
+			flattenedObjectives,
+			flattenedToString,
+			conditions,
+			metaNodeName,
+			1,
+			as...
+		),
+		false,
+		false
+	);
+}
