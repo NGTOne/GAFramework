0b9c5cef266fe392ac204b6136b85e8f4f71843a
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-07-09 11:51:47 -0400

    [Meta Example]: Updated to use new factories

--- src/examples/MetaHierLF.cpp
+++ src/examples/MetaHierLF.cpp
@@ -8,51 +8,21 @@ using namespace std;
 
 int main(void) {
 	HierarchicalEA ea(100);
-	vector<Locus*> baseLoci(8, new IntLocus(0, 1));
-	vector<PopulationNode*> bottomNodes;
-	vector<Locus*> topLoci;
 
-	for (int i = 0; i < 4; i++) {
-		stringstream name;
-		name << "P" << 2 + i;
-		bottomNodes.push_back(new EANode(
-			8,
-			baseLoci,
-			vector<ObjectiveFunction*>(),
-			new LongestFragmentToString(),
-			vector<EndCondition*>({new IterationCountEnd(100)}),
-			name.str(),
-			new GA(2, false, new TournamentSelection(0.95, 4)),
-			new NPointCrossover(2),
-			new UniformMutation(0.2)
-		));
-		topLoci.push_back(new PopulationLocus(bottomNodes[i]));
-	}
-
-	ea.addNodes(
-		bottomNodes,
-		vector<bool>(4, true),
-		vector<bool>(4, false)
-	);
-
-	// Add context for the top node (to demonstrate)
-	for (unsigned int i = 0; i < 16; i++)
-		topLoci.push_back(new IntLocus(0, 1));
-
-	ea.addNode(
-		new EANode(
-			8,
-			topLoci,
-			vector<ObjectiveFunction*>(),
-			new LongestFragmentToString(),
-			vector<EndCondition*>({new IterationCountEnd(100)}),
-			"P1",
-			new GA(2, false, new TournamentSelection(0.95, 4)),
-			new NPointCrossover(2),
-			new UniformMutation(0.2)
-		),
-		true,
-		true
+	ea.addConstructiveTree<EANode>(
+		new ConstructiveLocusCountPopFormula(),
+		{
+			vector<Locus*>(16, new IntLocus(0, 1)),
+			vector<Locus*>(8, new IntLocus(0, 1))
+		},
+		{new LongestFragmentToString(), new LongestFragmentToString()},
+		{{new IterationCountEnd(100)}, {new IterationCountEnd(100)}},
+		TreeBuilder("P1").addSubNodes("P1", {"P2", "P3", "P4", "P5"}),
+		{true, false},
+		{true, false},
+		new GA(2, false, new TournamentSelection(0.95, 4)),
+		new NPointCrossover(2),
+		new UniformMutation(0.2)
 	);
 
 	ea.addMetaPopulation<EANode>(
@@ -73,9 +43,6 @@ int main(void) {
 		new NPointCrossover(2),
 		new UniformMutation(0.2)
 	);
-
-	ea.setEvolutionOrder({"M1", "P5", "P4", "P3", "P2", "P1"});
-	ea.setNodesToPrint({"M1", "P1", "P2", "P3", "P4", "P5"});
 
 	ea.setEvolutionOrder({"M1", "P5", "P4", "P3", "P2", "P1"});
 	ea.setNodesToPrint({"M1", "P1", "P2", "P3", "P4", "P5"});
