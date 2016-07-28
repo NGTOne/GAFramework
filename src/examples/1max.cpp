e56b35911d74af9ba9f69a3ae1d8079fcf84e811
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-07-27 17:03:17 -0700

    [Examples]: Updated basic examples to use new idiom

--- src/examples/1max.cpp
+++ src/examples/1max.cpp
@@ -8,24 +8,22 @@ using namespace std;
 int main(void) {
 	HierarchicalEA ea(100);
 	vector<Locus*> baseLoci(32, new IntLocus(0, 1));
-	ea.addNode(
-		new EANode(
-			64,
-			baseLoci,
-			{new OneMaxFitness()},
-			new OneMaxToString(),
-			{new IterationCountEnd(100)},
-			"P1",
-			new GA(
-				2,
-				false,
-				new TournamentSelection(0.95, 4),
-				new NPointCrossover(2),
-				new UniformMutation(0.2)
-			)
-		),
+	ea.addNode<EANode>(
+		64,
+		baseLoci,
+		{new OneMaxFitness()},
+		new OneMaxToString(),
+		{new IterationCountEnd(100)},
+		"P1",
 		true,
-		true
+		true,
+		new GA(
+			2,
+			false,
+			new TournamentSelection(0.95, 4),
+			new NPointCrossover(2),
+			new UniformMutation(0.2)
+		)
 	);
 
 	ea.setEvolutionOrder({"P1"});
--- src/examples/LongestFragment.cpp
+++ src/examples/LongestFragment.cpp
@@ -9,24 +9,22 @@ int main(void) {
 	HierarchicalEA ea(100);
 	vector<Locus*> baseLoci(32, new IntLocus(0, 1));
 
-	ea.addNode(
-		new EANode(
-			64,
-			baseLoci,
-			{new LongestFragmentFitness()},
-			new LongestFragmentToString(),
-			{new IterationCountEnd(100)},
-			"P1",
-			new GA(
-				2,
-				false,
-				new TournamentSelection(0.95, 4),
-				new NPointCrossover(2),
-				new UniformMutation(0.2)
-			)
-		),
+	ea.addNode<EANode>(
+		64,
+		baseLoci,
+		{new LongestFragmentFitness()},
+		new LongestFragmentToString(),
+		{new IterationCountEnd(100)},
+		"P1",
 		true,
-		true
+		true,
+		new GA(
+			2,
+			false,
+			new TournamentSelection(0.95, 4),
+			new NPointCrossover(2),
+			new UniformMutation(0.2)
+		)
 	);
 	ea.setEvolutionOrder({"P1"});
 	ea.run(true);
