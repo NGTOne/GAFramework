a631b2a4ef891d8c0f2691f2d5bd416523490684
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-08-07 17:25:26 -0400

    [Locus]: Removed old code

--- src/core/Locus.cpp
+++ src/core/Locus.cpp
@@ -1,39 +1,9 @@
 #include "core/Locus.hpp"
-#include "core/HierRNG.hpp"
-#include "exception/ValueOutOfRangeException.hpp"
-#include <chrono>
-#include <sstream>
-
-using namespace std;
 
 Locus::Locus() {}
 
-Locus::Locus(vector<boost::any> population) {
-	this->population = population;
-}
-
-void Locus::setPopulation(vector<boost::any> population) {
-	this->population = population;
-}
-
-boost::any Locus::getIndex(unsigned int index) {
-	return this->population[index];
-}
-
 Locus::~Locus() {}
-
-unsigned int Locus::topIndex() {
-	return this->population.size() - 1;
-}
-
-unsigned int Locus::randomIndex() {
-	return HierRNG::index(this->topIndex());
-}
 
 bool Locus::isConstructive() {
 	return false;
-}
-
-bool Locus::outOfRange(unsigned int i) {
-	return i >= this->population.size();
 }
