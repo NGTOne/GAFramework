399573d1ffaa174962ce4cf970f48675f3f5406e
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-08-12 18:30:48 -0400

    [CSVInstrument]: Made CSVInstrument more robust

--- include/instruments/population/CSVInstrument.hpp
+++ include/instruments/population/CSVInstrument.hpp
@@ -16,10 +16,10 @@ class CSVInstrument: public PopulationInstrument {
 	CSVInstrument(PopulationNode* target, std::string outFile);
 
 	template <typename T>
-	void write(std::vector<T> values);
+	void write(std::vector<T> values, T defaultValue);
 
 	template <typename Val>
-	void write(std::map<std::string, Val> values);
+	void write(std::map<std::string, Val> values, Val defaultValue);
 
 	template <typename T>
 	void setHeader(std::vector<T> values);
@@ -28,9 +28,9 @@ class CSVInstrument: public PopulationInstrument {
 };
 
 template <typename T>
-void CSVInstrument::write(std::vector<T> values) {
+void CSVInstrument::write(std::vector<T> values, T defaultValue) {
 	if (!this->headerWritten) {
-		this->write(this->header);
+		this->write(this->header, std::string());
 		this->headerWritten = true;
 	}
 
@@ -39,17 +39,33 @@ void CSVInstrument::write(std::vector<T> values) {
 	for (unsigned int i = 0; i < values.size() - 1; i++)
 		ss << values[i] << ",";
 	ss << values[values.size() - 1];
+
+	if (values.size() < this->header.size()) {
+		for (
+			unsigned int i = values.size();
+			i < this->header.size() - 1;
+			i++
+		) ss << defaultValue << ",";
+		ss << defaultValue;
+	}
+
 	HierInstrument::write(ss.str());
 }
 
 template <typename Val>
-void CSVInstrument::write(std::map<std::string, Val> values) {
+void CSVInstrument::write(std::map<std::string, Val> values, Val defaultValue) {
 	std::vector<Val> valuesToWrite;
 
-	for (unsigned int i = 0; i < this->header.size(); i++)
-		valuesToWrite.push_back(values.find(this->header[i])->second);
+	for (unsigned int i = 0; i < this->header.size(); i++) {
+		auto it = values.find(this->header[i]);
+		if (it != values.end()) {
+			valuesToWrite.push_back(it->second);
+		} else {
+			valuesToWrite.push_back(defaultValue);
+		}
+	}
 
-	this->write(valuesToWrite);
+	this->write(valuesToWrite, defaultValue);
 }
 
 template <typename T>
