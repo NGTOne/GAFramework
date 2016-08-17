a894ea809a52fffc10949b1f8673a987a3d1e297
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-08-17 13:14:03 -0400

    [ES]: Reworked ES to be base class

--- include/nodes/EANode/systems/ES.hpp
+++ include/nodes/EANode/systems/ES.hpp
@@ -2,58 +2,33 @@
 #define NODES_EANODE_SYSTEMS_ES
 
 #include "../EvolutionarySystem.hpp"
-#include <random>
 #include <string>
 #include <vector>
-
-/*
-* The ES model uses the (mu/rho+, lambda)-ES model, which is an
-* extension of the original ES model proposed by Ingo Rechenberg et. al. in
-* the 1960s.
-*
-* In this model, the original population, the offspring created by crossover,
-* and the offspring created by mutation (which is done independently of
-* crossover) are all combined into one population and sorted, with the next
-* generation being the members of the combined population that are the
-* most fit.
-*/
 
 class ES : public EvolutionarySystem {
 	private:
 
 	protected:
-	double muRatio;
-	double rhoRatio;
-
-	void init(double muRatio, double rhoRatio);
-
-	Genome* getCrossoverChild(
-		std::vector<Genome*> initialPopulation,
-		std::vector<float> populationFitnesses,
-		std::string speciesNode
+	ES(MutationOperation* mutation);
+	ES(CrossoverOperation* cross, MutationOperation* mutation);
+	ES(
+		SelectionStrategy* strategy,
+		CrossoverOperation* cross,
+		MutationOperation* mutation
 	);
+
+	virtual std::vector<Genome*> getOffspring(
+		std::vector<Genome*> parents,
+		std::string speciesNode
+	)=0;
+	virtual std::vector<Genome*> doReplacement(
+		std::vector<Genome*> parents,
+		std::vector<Genome*> offspring,
+		std::vector<float>& populationFitnesses,
+		std::vector<ObjectiveFunction*> objectives
+	)=0;
 
 	public:
-	ES(CrossoverOperation * cross, MutationOperation * mutation);
-	ES(
-		SelectionStrategy* strategy,
-		CrossoverOperation* cross,
-		MutationOperation* mutation
-	);
-	ES(
-		double muRatio,
-		double rhoRatio,
-		CrossoverOperation* cross,
-		MutationOperation* mutation
-	);
-	ES(
-		double muRatio,
-		double rhoRatio,
-		SelectionStrategy* strategy,
-		CrossoverOperation* cross,
-		MutationOperation* mutation
-	);
-
 	std::vector<Genome*> breedMutateSelect(
 		std::vector<Genome*> initialPopulation,
 		std::vector<float>& populationFitnesses,
--- src/nodes/EANode/systems/ES.cpp
+++ src/nodes/EANode/systems/ES.cpp
@@ -1,68 +1,20 @@
-#include <random>
-#include <chrono>
-#include <string>
-#include <sstream>
 #include "nodes/EANode/systems/ES.hpp"
 #include "nodes/EANode/selections/RandomSelection.hpp"
-#include "core/utils/HierRNG.hpp"
+
+ES::ES(
+	MutationOperation* mutation
+) : EvolutionarySystem(new RandomSelection(), NULL, mutation) {}
 
 ES::ES(
 	CrossoverOperation* cross,
 	MutationOperation* mutation
-) : EvolutionarySystem(new RandomSelection(), cross, mutation) {
-	this->init(1, 1);
-}
+) : EvolutionarySystem(new RandomSelection(), cross, mutation) {}
 
 ES::ES(
 	SelectionStrategy* strategy,
 	CrossoverOperation* cross,
 	MutationOperation* mutation
-) : EvolutionarySystem(strategy, cross, mutation) {
-	this->init(1, 1);
-}
-
-ES::ES(
-	double muRatio,
-	double rhoRatio,
-	CrossoverOperation* cross,
-	MutationOperation* mutation
-) : EvolutionarySystem(new RandomSelection(), cross, mutation) {
-	this->init(muRatio, rhoRatio);
-}
-
-ES::ES(
-	double muRatio,
-	double rhoRatio,
-	SelectionStrategy* strategy,
-	CrossoverOperation* cross,
-	MutationOperation* mutation
-) : EvolutionarySystem(strategy, cross, mutation) {
-	this->init(muRatio, rhoRatio);
-}
-
-void ES::init(double muRatio, double rhoRatio) {
-	this->muRatio = muRatio;
-	this->rhoRatio = rhoRatio;
-}
-
-Genome* ES::getCrossoverChild(
-	std::vector<Genome*> initialPopulation,
-	std::vector<float> populationFitnesses,
-	std::string speciesNode
-) {
-	std::vector<Genome*> parents, children;
-	for (int i = 0; i < 2; i++)
-		parents.push_back(initialPopulation[this->getParent(
-			initialPopulation,
-			populationFitnesses
-		)]);
-
-	children = this->cross->crossOver(parents, speciesNode);
-
-	Genome* child = new Genome(HierRNG::choose(children));
-	for (unsigned int i = 0; i < children.size(); i++) delete(children[i]);
-	return child;
-}
+) : EvolutionarySystem(strategy, cross, mutation) {}
 
 std::vector<Genome*> ES::breedMutateSelect(
 	std::vector<Genome*> initialPopulation,
@@ -70,69 +22,18 @@ std::vector<Genome*> ES::breedMutateSelect(
 	std::vector<ObjectiveFunction*> objectives,
 	std::string speciesNode
 ) {
-	unsigned int initialPopSize = initialPopulation.size();
-	unsigned int numMutants = initialPopSize * rhoRatio;
-	unsigned int numCrossChildren = initialPopSize * muRatio;
-	std::vector<Genome*> crossChildren, mutantChildren, overallPopulation;
-	std::vector<float> finalPopulationFitnesses;
-
-	for (unsigned int i = 0; i < numMutants; i++)
-		mutantChildren.push_back(
-			this->mutation->mutate(initialPopulation[
-				this->getParent(
-					initialPopulation,
-					populationFitnesses
-				)
-			])
-		);
-
-	for (unsigned int i = 0; i < numCrossChildren; i++)
-		crossChildren.push_back(this->getCrossoverChild(
-			initialPopulation,
-			populationFitnesses,
-			speciesNode
-		));
-
-	for (unsigned int i = 0; i < initialPopSize; i++) {
-		overallPopulation.push_back(initialPopulation[i]);
-		finalPopulationFitnesses.push_back(populationFitnesses[i]);
-	}
-
-	for (unsigned int i = 0; i < numMutants; i++) {
-		overallPopulation.push_back(mutantChildren[i]);
-		finalPopulationFitnesses.push_back(
-			this->evaluateFitness(mutantChildren[i], objectives)
-		);
-	}
-
-	for (unsigned int i = 0; i < numCrossChildren; i++) {
-		overallPopulation.push_back(crossChildren[i]);
-		finalPopulationFitnesses.push_back(
-			this->evaluateFitness(crossChildren[i], objectives)
-		);
-	}
-
-	this->sortPopulation(
-		overallPopulation,
-		finalPopulationFitnesses
+	std::vector<Genome*> offspring = this->getOffspring(
+		initialPopulation,
+		speciesNode
 	);
-
-	std::vector<Genome*> finalPopulation;
-
-	//Since they're sorted, the best of the new generation can simply be
-	//pulled from the top of the list
-	for (unsigned int i = 0; i < initialPopSize; i++) {
-		finalPopulation.push_back(
-			new Genome(overallPopulation[i])
-		);
-		populationFitnesses[i] = finalPopulationFitnesses[i];
-	}
-
-	// A little housekeeping
-	for (unsigned int i = 0; i < mutantChildren.size(); i++)
-		delete(mutantChildren[i]);
-	for (unsigned int i = 0; i < crossChildren.size(); i++)
-		delete(crossChildren[i]);
-
-	return finalPopulation;
+	std::vector<Genome*> results = this->doReplacement(
+		offspring,
+		initialPopulation,
+		populationFitnesses,
+		objectives
+	);
+	std::vector<Genome*> finalResults;
+	for (auto genome: results) finalResults.push_back(new Genome(genome));
+	for (auto off: offspring) delete(off);
+	return finalResults;
 }
