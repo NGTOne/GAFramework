a200d8e982618ac91cc200e83c1029ba6a9ee55c
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2017-05-28 13:01:37 +0200

    [FitnessNoiseSource]: Updated to use FitnessSource

--- src/objectives/noisy/fitness-noise/FitnessNoiseSource.cpp
+++ src/objectives/noisy/fitness-noise/FitnessNoiseSource.cpp
@@ -7,8 +7,11 @@ Fitness FitnessNoiseSource::noisify(
 	Fitness cleanFitness,
 	std::function<double(double)> noisemaker
 ) {
-	std::vector<double> components = cleanFitness.getComponents();
+	std::vector<FitnessPair> components = cleanFitness.getComponents();
 	for (unsigned int i = 0; i < components.size(); i++)
-		components[i] = noisemaker(components[i]);
+		components[i] = std::make_tuple(
+			noisemaker(std::get<0>(components[i])),
+			std::get<1>(components[i])
+		);
 	return Fitness(components);
 }
