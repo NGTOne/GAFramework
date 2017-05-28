7cfbc46ff0ddc774627654fb40c4405252c5a8e4
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2017-05-28 11:35:28 +0200

    [FitnessSource]: Added new class for tracking "source" of fitness values

---
+++ include/core/FitnessSource.hpp
@@ -0,0 +1,22 @@
+#ifndef CORE_FitnessSource
+#define CORE_FitnessSource
+
+#include <boost/variant/variant.hpp>
+#include "eval/ObjectiveFunction.hpp"
+#include "Genome.hpp"
+
+class FitnessSource {
+	private:
+
+	protected:
+	boost::variant<ObjectiveFunction*, Genome*> source;
+
+	public:
+	FitnessSource(boost::variant<ObjectiveFunction*, Genome*> source);
+	const boost::variant<ObjectiveFunction*, Genome*> getSource() const;
+
+	bool operator==(const FitnessSource& rhs) const;
+	bool operator!=(const FitnessSource& rhs) const;
+};
+
+#endif
---
+++ src/core/FitnessSource.cpp
@@ -0,0 +1,22 @@
+#include "core/FitnessSource.hpp"
+
+FitnessSource::FitnessSource(
+	boost::variant<ObjectiveFunction*, Genome*> source
+) {
+	this->source = source;
+}
+
+const boost::variant<
+	ObjectiveFunction*,
+	Genome*
+> FitnessSource::getSource() const {
+	return this->source;
+}
+
+bool FitnessSource::operator==(const FitnessSource& rhs) const {
+	return rhs.getSource() == this->source;
+}
+
+bool FitnessSource::operator!=(const FitnessSource& rhs) const {
+	return !(*this == rhs);
+}
